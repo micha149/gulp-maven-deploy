22ef3661dab4972e9a2544e58388d2399d0a6a54
leftieFriele <leftie.friele@gmail.com>
2014-06-04 17:41:02 +0200

    fixing bug with callbacks, now it should work

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ var deploy = function(options, callback) {
 		return through.obj(function(file, enc, cb) {
 			gmd.config(options.config);
 			if (!options.config.hasOwnProperty('repositories')) {
-				callback(new Error('Missing repositories configuration'));
+				throw new Error('Missing repositories configuration');
 			}
 			options.config.repositories.forEach(function(repo) {
 				if (!repo.hasOwnProperty('id') || !repo.hasOwnProperty('url')) {
 					throw new Error('Deploy required "id" and "url".')
 				}
 				gmd.deploy(repo.id, repo.url);
-				callback(null);
+				if (callback)callback(null);
 			});
 		});
 	}
@@ -37,7 +37,7 @@ var install = function(options, callback) {
 			if (callback) callback(null);
 		});
 	}
-	if (callback) callback(new Error('Invalid configuration'));
+	throw new Error('Invalid configuration');
 }
 
 module.exports.deploy = deploy.bind(this);
