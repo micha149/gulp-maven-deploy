0d8cec602e8c0051e1de37a8df9f2e5f81e26b3f
Michael van Engelshoven <michael@van-engelshoven.de>
2016-02-28 07:35:28 +0100

    Implemented deploy of gulp files
    Previously alway files from a build directory were deployed,
    no matter whats in the gulp stream. This changes with this commit.
    Now each file from the gulp stream will be written to hard disk
    and passed to maven deploy.
    
    Fixes #8 and #12

--- test/deployTest.js
+++ test/deployTest.js
@@ -2,7 +2,8 @@ var plugin = require('../index.js'),
     expect = require('chai').expect,
     sinon = require('sinon'),
     Vinyl = require('vinyl'),
-    mavenDeploy = require('maven-deploy');
+    mavenDeploy = require('maven-deploy'),
+    fs = require('fs');
 
 /* globals describe: false, it: false, beforeEach: false, afterEach: false */
 
@@ -69,7 +70,57 @@ describe('gulp-maven-deploy plugin', function () {
             stream.on('finish', function() {
                 expect(mavenDeploy.deploy).to.be.calledTwice;
                 expect(mavenDeploy.deploy).to.be.calledWith(testConfig.repositories[0].id);
-                expect(mavenDeploy.deploy).to.be.calledWith(testConfig.repositories[0].id);
+                done();
+            });
+
+            stream.write(fileA);
+            stream.write(fileB);
+            stream.end();
+        });
+
+        it('calls deploy with a temporary file which has correct content', function (done) {
+            var stream = plugin.deploy(testConfig);
+
+            mavenDeploy.deploy.restore();
+            sinon.stub(mavenDeploy, 'deploy', function(repoId, filename, snapshot, callback) {
+                fs.readFile(filename, 'utf-8', function(err, content) {
+                    expect(content).to.be.equal(fileA.contents.toString());
+                    callback(null);
+                });
+            });
+
+            stream.on('finish', function() {
+                done();
+            });
+
+            stream.write(fileA);
+            stream.end();
+        });
+
+        it('removes temporary file when finished', function(done) {
+            var stream = plugin.deploy(testConfig);
+
+            stream.on('finish', function() {
+                fs.stat(mavenDeploy.deploy.firstCall.args[1], function(error) {
+                    expect(error.code).to.be.equal('ENOENT');
+                    done();
+                });
+            });
+
+            stream.write(fileA);
+            stream.end();
+        });
+
+        it('passes files to next stream handler', function(done) {
+            var stream = plugin.deploy(testConfig),
+                spy = sinon.spy();
+
+            stream.on('data', spy);
+
+            stream.on('finish', function() {
+                expect(spy).to.be.calledTwice;
+                expect(spy).to.be.calledWith(fileA);
+                expect(spy).to.be.calledWith(fileB);
                 done();
             });
 
