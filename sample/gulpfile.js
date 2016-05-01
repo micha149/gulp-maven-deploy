38b224164a98a8bba9130c4bc2dcc5344407d23d
Michael van Engelshoven <michael@van-engelshoven.de>
2016-05-01 12:30:14 +0200

    Fix example to new 1.0.0 code

--- sample/gulpfile.js
+++ sample/gulpfile.js
@@ -1,29 +1,25 @@
-var gulp = require('gulp');
-var maven = require('../index');
+var gulp = require('gulp'),
+    zip = require('gulp-zip'),
+    maven = require('../index');
 
 gulp.task('deploy-remote', function() {
     gulp.src('.')
+        .pipe(zip('sillysample.war'))
         .pipe(maven.deploy({
-            'config': {
-                'groupId': 'com.mygroup',
-                'type': 'war',
-                'repositories': [{
-                    'id': 'some-repo-id',
-                    'url': 'http://some-repo/url'
-                }]
-            }
+            'groupId': 'com.mygroup',
+            'repositories': [{
+                'id': 'some-repo-id',
+                'url': 'http://some-repo/url'
+            }]
         }));
 });
 
 gulp.task('deploy-local', function() {
     gulp.src('.')
+        .pipe(zip('sillysample.war'))
         .pipe(maven.install({
-            'config': {
-                'groupId': 'com.mygroup',
-                'type': 'war'
-            }
+            'groupId': 'com.mygroup',
         }));
 });
-
 
 gulp.task('default', ['deploy-local']);
